409e21dc9bac3c36c3faee16732cc9ba8350a68f
Elham Amini <elhamfarvid@gmail.com>
2019-09-30 18:42:19 -0400

    mostly done

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -1,8 +1,22 @@
 function isEqual(obj1, obj2) {
   // write your function here
-  // run your tests by running `jest isEqual`
-  // you can also run tests on every save by running
-  // `jest --watch isEqual`
+  // // run your tests by running `jest isEqual`
+  // // you can also run tests on every save by running
+  // // `jest --watch isEqual`
+  if(obj1==={}&&obj2==={}){
+    return true;
+  }
+  let keys1=Object.keys(obj1);
+  let keys2=Object.keys(obj2);
+  if(keys1.length!== keys2.length){
+    return false;
+  }
+  for(let key in obj1){
+    if(obj1[key]!==obj2[key]){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = { isEqual };
--- questions/01_largestDivisible/largestDivisible.js
+++ questions/01_largestDivisible/largestDivisible.js
@@ -4,7 +4,21 @@ write the largestDivisible function
 
 */
 
-function largestDivisible(arr, num) {}
+function largestDivisible(arr, num) {
+
+    if(arr.length===0 || num===0){
+        return null;
+
+    }
+    arr.forEach((target)=>{
+        if(target>num && target%num===0){
+            return target;
+        }
+
+    });
+    return null;
+    
+}
 
 // console.log(largestDivisible([1, 2, 3, 4, 5, 6, 7], 3));//6
 // console.log(largestDivisible([1], 2));//null
